07cae3ffe13d3984dee9deaa0f47c5073b3f8ef4
John Hunt <jeh@midmarsh.co.uk>
2020-04-14 11:12:03 +0100

    Simple Helloworld style application

---
+++ 01Bookshop/lib/src/main/java/com/jjh/android/lib/BookshopApp.java
@@ -0,0 +1,8 @@
+package com.jjh.android.lib;
+
+public class BookshopApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java Bookshop");
+        System.out.println("============================");
+    }
+}
