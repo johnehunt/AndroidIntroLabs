41a642cbb372efe0ab6755a9da6c5838c5be7ad1
John Hunt <jeh@midmarsh.co.uk>
2020-04-14 17:27:57 +0100

    Initial Import

--- 01Bookshop/lib/src/main/java/com/jjh/android/lib/BookshopApp.java
+++
@@ -1,8 +0,0 @@
-package com.jjh.android.lib;
-
-public class BookshopApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Java Bookshop");
-        System.out.println("============================");
-    }
-}
